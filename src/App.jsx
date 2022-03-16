ada6acaae8d95340a96b589f53008a42a83e16f6
Prodipta <haldip9500@gmail.com>
2022-03-17 02:37:03 +0530

    lighting and shadows added

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Three from './component/three';
 function App() {
 
   return (
-    <Canvas id="three-canvas-container">
+    <Canvas id="three-canvas-container" shadows>
       <Suspense fallback={null}>
         <Three/>
       </Suspense>
--- src/component/three/index.jsx
+++ src/component/three/index.jsx
@@ -1,7 +1,34 @@
-import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { Environment, OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import { angleToRadians } from '../../utils/angle';
+import {Physics, usePlane} from '@react-three/cannon';
+import * as THREE from 'three';
+
+function Ball(){
+
+    return(
+        <mesh 
+        position={[0, 0.5, 0]} castShadow>
+            <sphereGeometry args={[0.5, 32, 32]}/>
+            <meshPhongMaterial color="red" metalness={1} roughness={0.4}/>
+        </mesh>
+    );
+}
+
+function Plane(){
+    
+    const [ref] = usePlane(() => ({ rotation: [-(angleToRadians(90)), 0 , 0]}));
+
+    return(
+            <mesh 
+            ref={ref}
+            receiveShadow>
+                <planeGeometry args={[7, 7]}/>
+                <meshStandardMaterial color="#001eff"/>
+            </mesh>        
+    )
+}
 
 export default function Three(){
 
@@ -16,31 +43,44 @@ export default function Three(){
 
     })
 
-    // useEffect(()=>{
-    //     if(!!orbitControlsRef.current){
-    //         console.log(orbitControlsRef.current);
-    //     }
-    // },[orbitControlsRef.current])
+
+    useEffect(()=>{
+        if(!!orbitControlsRef.current){
+            console.log(orbitControlsRef.current);
+        }
+    },[orbitControlsRef.current])
 
     return (
         <>
             {/* Camera */}
             <PerspectiveCamera makeDefault position={[0, 1, 10]}/>
             <OrbitControls ref={orbitControlsRef} minPolarAngle={angleToRadians(40)} maxPolarAngle={angleToRadians(80)}/>
+            <Physics>
+                
+                <Ball/>
+                <Plane/>
 
-            {/* Ball */}
-            <mesh position={[0, 0.5, 0]}>
-                <sphereGeometry args={[0.5, 32, 32]}/>
-                <meshStandardMaterial color="red"/>
-            </mesh>
-
-            {/* Floor */}
-            <mesh rotation={[-(angleToRadians(90)), 0 , 0]}>
-                <planeGeometry args={[7, 7]}/>
-                <meshStandardMaterial color="#000000"/>
-            </mesh>
+            </Physics>
+            
             {/* Ambient light */}
-            <ambientLight args={["#ffffff", 1]}/>
+            <ambientLight args={["#ffffff", 0.5]}/>
+
+            {/* directional light */}
+            <directionalLight args={["#ffffff", 1]} position={[-3, 1, 0]}/>
+
+            {/* point Light */}
+            <pointLight args={["#ffffff", 1]} position={[-3, 1, 0]} castShadow/>
+
+            {/* spot Light */}
+            {/* <spotLight args={["#ffffff", 1.5, 10, angleToRadians(30), 0.2]} position={[-3, 1, 0]} castShadow/> */}
+
+            {/* Environment */}
+            <Environment background>
+                <mesh scale={100}>
+                    <sphereGeometry args={[1, 64, 64]}/>
+                    <meshBasicMaterial side={THREE.BackSide} color="#ff2e3a" />
+                </mesh>
+            </Environment>
         </>
     )
 }
